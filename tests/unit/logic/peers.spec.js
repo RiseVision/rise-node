2866a6df19e936e68a2034d0c13f3cf718383bd0
Alessio Scarapazzi <alessioscarapazzi@gmail.com>
2017-10-19 21:24:07 +0200

    Added unit-tests for logic/peers

--- tests/unit/logic/peers.spec.js
+++ tests/unit/logic/peers.spec.js
@@ -9,8 +9,6 @@ var rootDir = path.join(__dirname, "../../..");
 var Peers = rewire(path.join(rootDir, "logic/peers"));
 var Peer = require('../../../logic/peer');
 
-// foo = new Peers({}, function(){});
-// foo.create();
 
 describe("logic/peers", function() {
   var instance, callback, clock, schema, logger, peer;
@@ -18,7 +16,10 @@ describe("logic/peers", function() {
   beforeEach(function() {
     schema = {};
     logger = {
-      warn: sinon.stub()
+      warn: sinon.stub(),
+      debug: sinon.stub(),
+      trace: sinon.stub(),
+      info: sinon.stub()
     };
     callback = sinon.stub();
     clock = sinon.useFakeTimers("setImmediate");
@@ -66,7 +67,7 @@ describe("logic/peers", function() {
 
   describe("create", function() {
 
-    var create = Peers.__get__("Peers.prototype.create"); // todo: prototypes are not accesible?
+    var create = Peers.__get__("Peers.prototype.create");
 
     it("returns a new Peer", function(done) {
 
@@ -90,13 +91,13 @@ describe("logic/peers", function() {
   describe("exists", function() {
 
     var __private;
-    var exists = Peers.__get__("Peers.prototype.exists"); // todo: prototypes are not accesible?
+    var exists = Peers.__get__("Peers.prototype.exists");
 
     beforeEach(function(){
       __private = Peers.__get__("__private");
     });
     afterEach(function(){
-      Peers.__set__("__private", __private); //reset
+      Peers.__set__("__private", __private);
     });
 
     it("returns true", function(done) {
@@ -129,13 +130,13 @@ describe("logic/peers", function() {
   describe("get", function() {
 
     var __private;
-    var get = Peers.__get__("Peers.prototype.get"); // todo: prototypes are not accesible?
+    var get = Peers.__get__("Peers.prototype.get");
 
     beforeEach(function(){
       __private = Peers.__get__("__private");
     });
     afterEach(function(){
-      Peers.__set__("__private", __private); //reset
+      Peers.__set__("__private", __private);
     });
 
     it("returns peer", function(done) {
@@ -180,15 +181,20 @@ describe("logic/peers", function() {
 
   describe("upsert", function() {
 
-    var selfInstance, create;
-    var upsert = Peers.__get__("Peers.prototype.upsert"); // todo: prototypes are not accesible?
+    var selfInstance, create, modules, __private;
+    var upsert = Peers.__get__("Peers.prototype.upsert");
 
     beforeEach(function(){
       selfInstance = Peers.__get__("self");
+      __private = Peers.__get__("__private");
+      modules = Peers.__get__("modules");
+
       create = sinon.stub(selfInstance, "create");
     });
     afterEach(function(){
       create.restore();
+      Peers.__set__("__private", __private);
+      Peers.__set__("modules", modules);
     });
 
     it("upsert rejected", function(done) {
@@ -223,24 +229,255 @@ describe("logic/peers", function() {
       done();
     });
 
-    it("peer exists, forced update", function(done) {
+    it("peer exists, updates", function(done) {
 
       var exists = sinon.stub(selfInstance, "exists").returns(true);
+      var mockedPrivate = {
+        peers: {
+          test: {
+            update: sinon.stub()
+          }
+        }
+      };
+      var __private = Peers.__get__("__private");
+
+      Peers.__set__("__private", mockedPrivate);
+
       create.callsFake(function(){
-        return {string: '1'}
+        return {string: 'test'}
       });
-      var retVal = upsert({}, true);
+      var retVal = upsert({});
 
       expect(create.calledOnce).to.equal(true);
       expect(create.getCall(0).args.length).to.equal(1);
       expect(create.getCall(0).args[0]).to.deep.equal({});
-      expect(retVal).to.equal(false);
+      expect(logger.debug.calledOnce).to.equal(true);
+      expect(logger.debug.getCall(0).args.length).to.equal(2);
+      expect(logger.debug.getCall(0).args[0]).to.equal('Updated peer test');
+      expect(retVal).to.equal(true);
 
       exists.restore();
       done();
     });
 
+    it("peer doesn't exists, rejects unacceptable peer on insert", function(done) {
 
+      var exists = sinon.stub(selfInstance, "exists").returns(false);
+      var mockedPrivate = {
+        peers: {
+          test: {
+            update: sinon.stub()
+          }
+        }
+      };
+      var peersMocked = {
+        peers: {
+          acceptable: sinon.stub()
+        }
+      };
+
+      Peers.__set__("__private", mockedPrivate);
+      Peers.__set__("modules", peersMocked);
+
+      create.callsFake(function(){
+        return {string: 'test'}
+      });
+      var retVal = upsert({});
+
+      expect(create.calledOnce).to.equal(true);
+      expect(create.getCall(0).args.length).to.equal(1);
+      expect(create.getCall(0).args[0]).to.deep.equal({});
+      expect(logger.debug.calledOnce).to.equal(true);
+      expect(logger.debug.getCall(0).args.length).to.equal(2);
+      expect(logger.debug.getCall(0).args[0]).to.equal('Rejecting unacceptable peer');
+      expect(retVal).to.equal(true);
+
+      exists.restore();
+      done();
+    });
+
+    it("peer doesn't exists, inserts user", function(done) {
+
+      var exists = sinon.stub(selfInstance, "exists").returns(false);
+      var __private = Peers.__get__("__private");
+      var modules = Peers.__get__("modules");
+      var mockedPrivate = {
+        peers: {
+          test: {
+            update: sinon.stub()
+          }
+        }
+      };
+      var peersMocked = {
+        peers: {
+          acceptable: sinon.stub().returns([1,2,3])
+        }
+      };
+
+      Peers.__set__("__private", mockedPrivate);
+      Peers.__set__("modules", peersMocked);
+
+      create.callsFake(function(){
+        return {string: 'test'}
+      });
+      var retVal = upsert({});
+
+      expect(create.calledOnce).to.equal(true);
+      expect(create.getCall(0).args.length).to.equal(1);
+      expect(create.getCall(0).args[0]).to.deep.equal({});
+      expect(logger.debug.calledOnce).to.equal(true);
+      expect(logger.debug.getCall(0).args.length).to.equal(2);
+      expect(logger.debug.getCall(0).args[0]).to.equal('Inserted new peer');
+      expect(retVal).to.equal(true);
+
+      exists.restore();
+      done();
+    });
+
+  });
+
+  describe("remove", function() {
+
+    var __private, create, selfInstance, exists;
+    var remove = Peers.__get__("Peers.prototype.remove");
+
+    beforeEach(function(){
+      __private = Peers.__get__("__private");
+      selfInstance = Peers.__get__("self");
+      create = sinon.stub(selfInstance, "create");
+      exists = sinon.stub(selfInstance, "exists");
+    });
+    afterEach(function(){
+      Peers.__set__("__private", __private);
+      create.restore();
+      exists.restore();
+    });
+
+    it("peer not found", function(done) {
+
+      var mockedPrivate = {
+        peers: {
+          test: {}
+        }
+      };
+      Peers.__set__("__private", mockedPrivate);
+      create.callsFake(function(){
+        return {string: '1'}
+      });
+      exists.returns(false);
+      var retVal = remove({});
+
+      expect(create.calledOnce).to.equal(true);
+      expect(create.getCall(0).args.length).to.equal(1);
+      expect(create.getCall(0).args[0]).to.deep.equal({});
+      expect(exists.calledOnce).to.equal(true);
+      expect(exists.getCall(0).args.length).to.equal(1);
+      expect(exists.getCall(0).args[0]).to.deep.equal({string: '1'});
+      expect(retVal).to.equal(false);
+
+      done();
+    });
+    it("peer found", function(done) {
+
+      var mockedPrivate = {
+        peers: {
+          test: {}
+        }
+      };
+      Peers.__set__("__private", mockedPrivate);
+      create.callsFake(function(){
+        return {string: '1'}
+      });
+      exists.returns(true);
+      var retVal = remove({});
+
+      expect(create.calledOnce).to.equal(true);
+      expect(create.getCall(0).args.length).to.equal(1);
+      expect(create.getCall(0).args[0]).to.deep.equal({});
+      expect(exists.calledOnce).to.equal(true);
+      expect(exists.getCall(0).args.length).to.equal(1);
+      expect(exists.getCall(0).args[0]).to.deep.equal({string: '1'});
+      expect(retVal).to.equal(true);
+
+      done();
+    });
+  });
+
+  describe("list", function() {
+
+    var __private;
+    var list = Peers.__get__("Peers.prototype.list");
+
+    beforeEach(function(){
+      __private = Peers.__get__("__private");
+    });
+    afterEach(function(){
+      Peers.__set__("__private", __private);
+    });
+
+    it("normalize false", function(done) {
+
+      var mockedPrivate = {
+        peers: {
+          test: {
+            object: function() {return true}
+          }
+        }
+      };
+      Peers.__set__("__private", mockedPrivate);
+
+      var retVal = list(true);
+
+      expect(retVal).to.deep.equal([true]);
+
+      done();
+    });
+    it("normalize true", function(done) {
+
+      var mockedPrivate = {
+        peers: {
+          test: {
+            something: true,
+            awesome: true
+          }
+        }
+      };
+      Peers.__set__("__private", mockedPrivate);
+
+      var retVal = list(false);
+
+      expect(retVal).to.deep.equal([
+        {
+          something: true,
+          awesome: true
+        }
+      ]);
+
+      done();
+    });
+  });
+
+  describe("bindModules", function() {
+
+    var bindModules = Peers.__get__("Peers.prototype.bindModules");
+
+    it("binds modules", function(done) {
+
+      var modules;
+      var modulesToBind = {
+        peers: [1, 2]
+      };
+      var expectedModules = {
+        peers: modulesToBind.peers
+      };
+
+      bindModules(modulesToBind);
+      modules = Peers.__get__("modules");
+
+      expect(modules).to.deep.equal(expectedModules);
+
+      done();
+    });
   });
 
 });
