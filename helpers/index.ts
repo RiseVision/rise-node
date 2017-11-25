0068f66b88ad0e3d75d83e2adbefe073c9bc32b6
Andrea Baccega <vekexasia@gmail.com>
2017-11-25 17:26:31 +0100

    created indexes.

---
+++ helpers/index.ts
@@ -0,0 +1,23 @@
+import BigNum from './bignum';
+import * as cache from './cache';
+import * as config from './config';
+import constants from './constants';
+import * as Database from './database';
+import * as Diff from './diff';
+import * as Inserts from './inserts.js';
+
+export * from './checkIpInList';
+export * from './ed';
+export * from './forkTypes';
+export * from './genericUtils';
+export * from './git';
+export * from './jobsQueue';
+export * from './orderBy';
+export * from './promiseUtils';
+export * from './RoundChanges';
+export * from './sequence';
+export * from './slots';
+export * from './transactionTypes';
+
+
+export {BigNum, cache, config, constants, Database, Diff, Inserts};
---
+++ logic/index.ts
@@ -0,0 +1,11 @@
+export * from './account';
+export * from './block';
+export * from './blockReward';
+export * from './broadcaster';
+export * from './peer';
+export * from './peers';
+export * from './round';
+export * from './transaction';
+export * from './transactionPool';
+
+
