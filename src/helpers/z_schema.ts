228cdccf621c948d1c68cde1217f1a43d71b75bd
Andrea Baccega <vekexasia@gmail.com>
2018-01-01 12:02:51 +0100

    [FIX] number casting was also creating undefined elements while orig object should be the same.

--- src/helpers/z_schema.ts
+++ src/helpers/z_schema.ts
@@ -107,12 +107,13 @@ export {z_schema};
 
 export function castFieldsToNumberUsingSchema(schema: any, obj: any) {
   if (typeof(obj) === 'undefined') {
-    return obj;
+    return;
   }
   if (schema.type === 'integer') {
     return parseInt(obj, 10);
   } else if (schema.type === 'object') {
     Object.keys(schema.properties)
+      .filter((k) => typeof(obj[k]) !== 'undefined')
       .forEach((k) => obj[k] = castFieldsToNumberUsingSchema(schema.properties[k], obj[k]));
   } else if (schema.type === 'array' && Array.isArray(obj)) {
     obj.forEach((item, idx) => {
