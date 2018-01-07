68c49aca90322fbe5bf7c4ea3f29a430ed382683
Matteo Canever <mcanever@gmail.com>
2018-01-07 20:43:32 +0100

    [In progress] tests/unit/block.spec.ts tests/stubs/BlockRewardLogicStub.ts; tests/stubs/TransactionLogicStub.ts;

--- tests/stubs/index.ts
+++ tests/stubs/index.ts
@@ -1,9 +1,13 @@
+import BlockRewardLogicStub from './BlockRewardLogicStub';
 import DbStub from './DbStub';
 import LoggerStub from './LoggerStub';
+import TransactionLogicStub from './TransactionLogicStub';
 import ZSchemaStub from './ZSchemaStub';
 
 export {
+  BlockRewardLogicStub,
   DbStub,
   LoggerStub,
-  ZSchemaStub
+  TransactionLogicStub,
+  ZSchemaStub,
 };
---
+++ tests/unit/logic/block.spec.ts
@@ -0,0 +1,350 @@
+import * as ByteBuffer from 'bytebuffer';
+import * as chai from 'chai';
+import * as crypto from 'crypto';
+import * as rewire from 'rewire';
+import * as sinon from 'sinon';
+import { SinonSpy } from 'sinon';
+import { Ed } from '../../../src/helpers';
+import { BlockLogic } from '../../../src/logic';
+import { BlockRewardLogicStub, TransactionLogicStub, ZSchemaStub } from '../../stubs';
+
+// FIXME problem with import of this library
+const assertArrays = require('chai-arrays');
+
+const { expect } = chai;
+chai.use(assertArrays);
+
+const ed = new Ed();
+const RewireBlock = rewire('../../../src/logic/block.ts');
+const passphrase = 'oath polypody manumit effector half sigmoid abound osmium jewfish weed sunproof ramose';
+const dummyKeypair = ed.makeKeypair(
+  crypto.createHash('sha256').update(passphrase, 'utf8').digest()
+);
+
+describe('logic/block', () => {
+  let dummyBlock;
+  let dummyTransactions;
+  let callback;
+  let instance;
+  let data;
+  let blockRewardLogicStub: BlockRewardLogicStub;
+  let zschemastub: ZSchemaStub;
+  let transactionLogicStub: TransactionLogicStub;
+  let createHashSpy: SinonSpy;
+
+  const bb = new ByteBuffer(1 + 4 + 32 + 32 + 8 + 8 + 64 + 64, true);
+  bb.writeInt(123);
+  bb.flip();
+  const buffer = bb.toBuffer();
+
+  beforeEach(() => {
+    const rewiredCrypto = RewireBlock.__get__('crypto');
+    createHashSpy = sinon.spy(rewiredCrypto, 'createHash');
+    dummyTransactions = [
+      {
+        type: 0,
+        amount: 108910891000000,
+        fee: 5,
+        timestamp: 0,
+        recipientId: '15256762582730568272R',
+        senderId: '14709573872795067383R',
+        senderPublicKey: '35526f8a1e2f482264e5d4982fc07e73f4ab9f4794b110ceefecd8f880d51892',
+        signature: 'f8fbf9b8433bf1bbea971dc8b14c6772d33c7dd285d84c5e6c984b10c4141e9fa56ace' +
+                   '902b910e05e98b55898d982b3d5b9bf8bd897083a7d1ca1d5028703e03',
+        id: '8139741256612355994',
+      },
+      {
+        type: 0,
+        amount: 108910891000000,
+        fee: 3,
+        timestamp: 0,
+        recipientId: '6781920633453960895R',
+        senderId: '14709573872795067383R',
+        senderPublicKey: '35526f8a1e2f482264e5d4982fc07e73f4ab9f4794b110ceefecd8f880d51892',
+        signature: 'e26edb739d93bb415af72f1c288b06560c0111c4505f11076ca20e2f6e8903d3b00730' +
+                   '9c0e04362bfeb8bf2021d0e67ce3c943bfe0c0193f6c9503eb6dfe750c',
+        id: '16622990339377112127',
+      },
+    ];
+
+    dummyBlock = {
+      version: 0,
+      totalAmount: 217821782000000,
+      totalFee: 8,
+      reward: 30000000,
+      payloadHash: 'b3cf5bb113442c9ba61ed0a485159b767ca181dd447f5a3d93e9dd73564ae762',
+      timestamp: 1506889306558,
+      numberOfTransactions: 2,
+      payloadLength: 8,
+      previousBlock: '1',
+      generatorPublicKey: 'c950f1e6c91485d2e6932fbd689bba636f73970557fe644cd901a438f74883c5',
+      transactions: dummyTransactions,
+      blockSignature: '8c5f2b088eaf0634e1f6e12f94a1f3e871f21194489c76ad2aae5c1b71acd848bc7b' +
+                      '158fa3b827e97f3f685c772bfe1a72d59975cbd2ccaa0467026d13bae50a',
+    };
+
+    callback = sinon.spy();
+    zschemastub = new ZSchemaStub();
+    blockRewardLogicStub = new BlockRewardLogicStub();
+    instance = new BlockLogic();
+    transactionLogicStub = new TransactionLogicStub();
+
+    // Inject dependencies!
+    (instance as any).zschema = zschemastub;
+    (instance as any).blockReward = blockRewardLogicStub;
+    (instance as any).ed = ed;
+    (instance as any).transaction = transactionLogicStub;
+
+    // Default stub configuration
+    blockRewardLogicStub.stubConfig.calcReward.return = 100000;
+    transactionLogicStub.stubConfig.getBytes.return = buffer;
+
+    data = {
+      keypair: dummyKeypair,
+      timestamp: Date.now(),
+      transactions: dummyTransactions,
+      previousBlock: { id: '1', height: 10 },
+    };
+  });
+
+  afterEach(() => {
+    callback.reset();
+    createHashSpy.restore();
+  });
+
+  describe('create', () => {
+    it('should return a new block', () => {
+      const newBlock = instance.create(data);
+      expect(newBlock).to.be.an.instanceof(Object);
+      expect(newBlock.totalFee).to.equal(8);
+      expect(newBlock.numberOfTransactions).to.equal(2);
+      expect(newBlock.transactions).to.have.lengthOf(2);
+    });
+
+    it('should call blockReward.calcReward', () => {
+      const newBlock = instance.create(data);
+      expect(blockRewardLogicStub.stubs.calcReward.calledOnce).to.be.true;
+    });
+
+    it('should call transaction.getBytes per each tx', () => {
+      const newBlock = instance.create(data);
+      expect(transactionLogicStub.stubs.getBytes.callCount).to.equal(2);
+    });
+
+    it('should call crypto.createHash', () => {
+      const newBlock = instance.create(data);
+      expect(createHashSpy.called).to.be.true;
+    });
+
+    it('should call this.sign and this.objectNormalize', () => {
+      const signSpy = sinon.spy(instance, 'sign');
+      const objectNormalizeSpy = sinon.spy(instance, 'objectNormalize');
+      const newBlock = instance.create(data);
+      expect(signSpy.called).to.be.true;
+      expect(objectNormalizeSpy.called).to.be.true;
+      signSpy.restore();
+      objectNormalizeSpy.restore();
+    });
+  });
+
+  describe('sign', () => {
+    it('should return a block signature with 128 of length', () => {
+      const blockSignature = instance.sign(dummyBlock, dummyKeypair);
+      expect(blockSignature).to.have.lengthOf(128);
+    });
+
+    it('should call ed.sign', () => {
+      const signSpy = sinon.spy(ed, 'sign');
+      const blockSignature = instance.sign(dummyBlock, dummyKeypair);
+      expect(signSpy.calledOnce).to.be.true;
+      signSpy.restore();
+    });
+  });
+
+  describe('[static] getBytes', () => {
+    it('should return a Buffer', () => {
+      expect(BlockLogic.getBytes(dummyBlock)).to.be.an.instanceof(Buffer);
+    });
+
+    it('should return a Buffer of a given length', () => {
+      expect(BlockLogic.getBytes(dummyBlock).length).to.lte(4 + 4 + 8 + 4 + 4 + 8 + 8 + 4 + 4 + 4 + 32 + 32 + 64);
+    });
+  });
+
+  describe('[static] getHash', () => {
+    it('should return a hash of Uint8Array type', () => {
+      const hash = BlockLogic.getHash(dummyBlock);
+      expect(hash).to.be.an.instanceof(Uint8Array);
+      expect(hash).to.be.ofSize(32);
+      const dummyHash = Uint8Array.from(
+        [ 33, 231, 109, 34, 81, 45, 206, 26, 221, 6, 171, 168, 208, 242, 96, 79,
+                 166, 77, 243, 219, 78, 12, 172, 171, 166, 123, 127, 92, 0, 242, 227, 135 ]
+      );
+      expect(hash).to.be.equalTo(dummyHash as any);
+    });
+
+    it('should call crypto.createHash', () => {
+      const hash = BlockLogic.getHash(dummyBlock);
+      expect(createHashSpy.calledOnce).to.be.true;
+    });
+  });
+
+  describe('[static] verifySignature', () => {
+    it('should call ed.verify and return the same result', () => {
+      const verifySpy = sinon.spy(ed, 'verify');
+      const signed = instance.create(data);
+      const verified = instance.verifySignature(signed);
+      expect(verifySpy.calledOnce).to.be.true;
+      expect(verifySpy.firstCall.returnValue).to.be.equal(verified);
+      verifySpy.restore();
+    });
+
+    it('should call BlockLogic.getHash', () => {
+      const signed = instance.create(data);
+      const getHashSpy = sinon.spy(BlockLogic, 'getHash');
+      const verified = instance.verifySignature(signed);
+      expect(getHashSpy.calledOnce).to.be.true;
+      expect(getHashSpy.firstCall.args[0]).to.be.deep.eq(signed);
+      getHashSpy.restore();
+    });
+  });
+
+  // describe("dbSave", () => {
+  //   it("returns an object", () => {
+  //     clock.tick();
+  //     var instance = callback.args[0][1];
+  //     var data = instance.dbSave(dummyBlock);
+  //     expect(data).to.be.an.instanceof(Object);
+  //     expect(data.table).to.equal("blocks");
+  //     expect(data.fields).to.be.equalTo([
+  //       "id",
+  //       "version",
+  //       "timestamp",
+  //       "height",
+  //       "previousBlock",
+  //       "numberOfTransactions",
+  //       "totalAmount",
+  //       "totalFee",
+  //       "reward",
+  //       "payloadLength",
+  //       "payloadHash",
+  //       "generatorPublicKey",
+  //       "blockSignature"
+  //     ]);
+  //     expect(data.values).to.have.all.keys([
+  //       "id",
+  //       "version",
+  //       "timestamp",
+  //       "height",
+  //       "previousBlock",
+  //       "numberOfTransactions",
+  //       "totalAmount",
+  //       "totalFee",
+  //       "reward",
+  //       "payloadLength",
+  //       "payloadHash",
+  //       "generatorPublicKey",
+  //       "blockSignature"
+  //     ]);
+  //   });
+  // });
+  //
+  // describe("dbSave() with wrong parameters", () => {
+  //   it("returns an exception", () => {
+  //     clock.tick();
+  //     var instance = callback.args[0][1];
+  //     wrongBlock = {};
+  //     var error = () => {
+  //       instance.dbSave(wrongBlock);
+  //     };
+  //     expect(error).to.throw();
+  //   });
+  // });
+  //
+  // describe("objectNormalize", () => {
+  //   it("returns a normalized block", () => {
+  //     clock.tick();
+  //     var instance = callback.args[0][1];
+  //     dummyBlock.foo = null;
+  //     dummyBlock.bar;
+  //     var block = instance.objectNormalize(dummyBlock);
+  //     expect(block).to.be.an.instanceof(Object);
+  //     expect(block.foo).to.be.undefined;
+  //     expect(block.bar).to.be.undefined;
+  //     expect(block.greeting).to.be.undefined;
+  //   });
+  // });
+  //
+  // describe("objectNormalize() with a bad block schema", () => {
+  //   // it("throws an exception", () => {
+  //   //   clock.tick();
+  //   //   var instance = callback.args[0][1];
+  //   //   dummyBlock.greeting = "Hello World!";
+  //   //   var throwError = () => {
+  //   //     instance.objectNormalize(dummyBlock);
+  //   //   };
+  //   //   expect(throwError).to.throw("Failed to validate block schema");
+  //   // });
+  // });
+  //
+  // describe("[static] getId", () => {
+  //   it("returns an id string", () => {
+  //     expect(Block.BlockLogic.getId(dummyBlock)).to.equal("1931531116681750305");
+  //   });
+  // });
+  //
+  // // TODO: This test is temporarely commented, because calculateFee() seems to be dead code. Waiting for a decision to be taken
+  // // describe('calculateFee()', function () {
+  // //   it('returns an integer', function () {
+  // //     var instance = callback.args[0][1]
+  // //     var fees = instance.calculateFee({})
+  // //     expect(fees).to.satisfy(Number.isInteger)
+  // //     expect(fees).to.equal(10000000)
+  // //   })
+  // // })
+  //
+  // describe("dbRead", () => {
+  //   it("returns an object", () => {
+  //     clock.tick();
+  //     var instance = callback.args[0][1];
+  //     var raw = {
+  //       b_id: 10,
+  //       b_version: 11,
+  //       b_timestamp: Date.now(),
+  //       b_height: 12,
+  //       b_previousBlock: 9,
+  //       b_numberOfTransactions: 1,
+  //       b_totalAmount: 0,
+  //       b_totalFee: 100,
+  //       b_reward: 50,
+  //       b_payloadLength: 13,
+  //       b_payloadHash: 14,
+  //       b_generatorPublicKey:
+  //         "c950f1e6c91485d2e6932fbd689bba636f73970557fe644cd901a438f74883c5",
+  //       b_blockSignature: 16,
+  //       b_confirmations: 1
+  //     };
+  //     var block = instance.dbRead(raw);
+  //     expect(block).to.be.instanceof(Object);
+  //     expect(block).to.have.all.keys([
+  //       "id",
+  //       "version",
+  //       "timestamp",
+  //       "height",
+  //       "previousBlock",
+  //       "numberOfTransactions",
+  //       "totalAmount",
+  //       "totalFee",
+  //       "reward",
+  //       "payloadLength",
+  //       "payloadHash",
+  //       "generatorPublicKey",
+  //       "generatorId",
+  //       "blockSignature",
+  //       "confirmations",
+  //       "totalForged"
+  //     ]);
+  //     expect(block.totalForged).to.equal("150");
+  //   });
+  // });
+});
