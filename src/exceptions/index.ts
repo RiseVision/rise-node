adb87ba5313e7bf79b057802941c444e09278eb0
Andrea Baccega <vekexasia@gmail.com>
2018-01-14 11:07:54 +0100

    no exceptions needed in testnet.

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -1,14 +1,6 @@
 import { ExceptionsManager } from '../helpers';
-import block127765 from './block_127765';
-import tx_10425551571020716913 from './tx_10425551571020716913';
-import tx_14712341342146176146 from './tx_14712341342146176146';
-import tx_1563714189640390961 from './tx_1563714189640390961';
 
 const allExceptionCreator: Array<(exc: ExceptionsManager) => void> = [
-  block127765,
-  tx_14712341342146176146,
-  tx_1563714189640390961,
-  tx_10425551571020716913,
 ];
 
 export { allExceptionCreator };
