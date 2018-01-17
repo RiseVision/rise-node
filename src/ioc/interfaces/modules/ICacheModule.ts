afacea2671b8bb2bd710e6215a985c2615eb5b16
Andrea Baccega <vekexasia@gmail.com>
2018-01-17 17:12:48 +0100

    Remove nonInterface methods from cachemodule..

--- src/ioc/interfaces/modules/ICacheModule.ts
+++ src/ioc/interfaces/modules/ICacheModule.ts
@@ -21,36 +21,4 @@ export interface ICacheModule extends IModule {
 
   quit(): Promise<void>;
 
-  /**
-   * This function will be triggered on new block, it will clear all cache entires.
-   * @param {Block} block
-   * @param {Broadcast} broadcast
-   * @param {Function} cb
-   */
-  onNewBlock(block, broadcast, cb): Promise<void>;
-
-  /**
-   * This function will be triggered when a round finishes, it will clear all cache entires.
-   * @param {Round} round
-   * @param {Function} cb
-   */
-  onFinishRound(round, cb): Promise<void>;
-
-  /**
-   * This function will be triggered when transactions are processed, it will clear all cache entires if there is a
-   * delegate type transaction.
-   * @param {Transactions[]} transactions
-   * @param {Function} cb
-   */
-  onTransactionsSaved(transactions: Array<IBaseTransaction<any>>, cb): Promise<void>;
-
-  /**
-   * Disable any changes in cache while syncing
-   */
-  onSyncStarted(): void;
-
-  /**
-   * Enable changes in cache after syncing finished
-   */
-  onSyncFinished(): void;
 }
