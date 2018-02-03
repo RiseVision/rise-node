b2db66a2ffa7a6399bb469c83af847acba93b5a9
Andrea Baccega <vekexasia@gmail.com>
2018-02-03 19:09:41 +0100

    fix #72

--- tests/unit/helpers/diff.spec.ts
+++ tests/unit/helpers/diff.spec.ts
@@ -1,13 +1,38 @@
+import { expect } from 'chai';
+import { merge, reverse } from '../../../src/helpers/diff';
 
 describe('helpers/diff', () => {
   describe('merge', () => {
-    it('should only add new content');
-    it('should only remove already in place content');
-    it('should return false if trying to remove not existing content');
-    it('should return false if trying to add already added content');
+    it('should only add new content', () => {
+      expect(merge(['b'], ['+a'])).to.be.deep.eq(['b', 'a']);
+    });
+    it('should only remove already in place content', () => {
+      expect(merge(['b', 'a'], ['-a'])).to.be.deep.eq(['b']);
+    });
+    it('should return false if trying to remove not existing content', () => {
+      expect(merge(['b'], ['-a'])).to.be.deep.eq(false);
+    });
+    it('should return false if trying to add already added content', () => {
+      expect(merge(['b'], ['+b'])).to.be.deep.eq(false);
+    });
+    it('should handle null source', () => {
+      expect(merge(null, ['+b']))
+        .to.be.deep.eq(['b']);
+    });
+    it('shouldnt modify original array', () => {
+      const arr     = ['a', 'b', 'c'];
+      const arrCopy = ['a', 'b', 'c'];
+      merge(arr, ['-a', '-b', '+d']);
+      expect(arr).to.be.deep.eq(arrCopy);
+    });
   });
 
   describe('reverse', () => {
-    it('should reverse diff output');
+    it('should reverse diff', () => {
+      expect(reverse(['-a', '+b', '-c', '+d'])).to.be.deep.eq(['+a', '-b', '+c', '-d']);
+    });
+    it('should return empty array if empty arr is given', () => {
+      expect(reverse([])).to.be.deep.eq([]);
+    });
   });
 });
