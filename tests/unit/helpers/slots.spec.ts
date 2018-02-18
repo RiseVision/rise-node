31af95edebbecce65f3cbd80f2a419d706780aa2
Andrea Baccega <vekexasia@gmail.com>
2018-02-18 10:48:21 +0100

    fixed some tests for testnet

--- tests/unit/helpers/slots.spec.ts
+++ tests/unit/helpers/slots.spec.ts
@@ -9,22 +9,23 @@ describe('helpers/slots', () => {
   (slots as any).constants = constants;
 
   // Test case: 3 hours after epochTime
-  const testDate = new Date(constants.epochTime.getTime() + 3 * 3600 * 1000);
+  const testDate = new Date(constants.epochTime.getTime() + 9 + 27 * 100 * 1000);
   const testTimestamp = testDate.getTime();
 
   it('getTime() should return the number of seconds elapsed since epochTime to the passed timestamp', () => {
     const retVal = slots.getTime(testTimestamp);
-    expect(retVal).to.be.eq( 3 * 3600 );
+    expect(retVal).to.be.eq( 27 * 100 );
   });
 
   it('getSlotNumber() should return the number of slot based on time given', () => {
     const retVal = slots.getSlotNumber(testTimestamp);
-    expect(retVal).to.be.eq( 48804000000 );
+    expect(retVal).to.be.eq( 54226366667 );
+    expect(slots.getSlotTime(retVal)).to.be.eq(testTimestamp);
   });
 
   it('getSlotTime() should return the timestamp for a specific SlotNumber', () => {
     // Just one slot later
-    const retVal = slots.getSlotTime(48804000001 );
+    const retVal = slots.getSlotTime(54226366667 + 1 );
     expect(retVal).to.be.eq( testTimestamp + constants.blockTime );
   });
 
