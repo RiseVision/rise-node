54f550a2e300c43413a33723bea8ea9066889e99
Matteo Canever <mcanever@gmail.com>
2018-02-28 01:39:17 +0100

    80% of RoundsModule unit tests. ( #101 )

--- tests/unit/modules/rounds.spec.ts
+++ tests/unit/modules/rounds.spec.ts
@@ -52,8 +52,7 @@ describe('modules/rounds', () => {
   let roundLogicScope: RoundLogicScope;
   let txGenerator: (scope: RoundLogicScope) => (task: any) => Promise<any>;
   let txGeneratorScoped: (task: any) => Promise<any>;
-  let afterTxPromise: (task: any) => () => Promise<any>;
-  let afterTxPromiseScoped: () => Promise<any>;
+  let afterTxPromise: () => () => Promise<any>;
   let innerTickStub: SinonStub;
 
   before(() => {
@@ -137,7 +136,7 @@ describe('modules/rounds', () => {
     innerTickStub   = sandbox.stub(instance as any, 'innerTick');
     // Expose the passed txGenerator so we can test it
     innerTickStub.callsFake((blk, backwards, txGen, afterTx = () => Promise.resolve(null)) => {
-      txGenerator = txGen;
+      txGenerator    = txGen;
       afterTxPromise = afterTx;
       return Promise.resolve('innerTick DONE');
     });
@@ -343,56 +342,343 @@ describe('modules/rounds', () => {
       });
 
       describe('then, if this was the last block in round', () => {
-        it('should call roundLogic.land', async () => {
+        beforeEach(() => {
           roundLogicScope.finishRound = true;
+        });
+
+        it('should call roundLogic.land', async () => {
           await doCall();
           expect(roundLogicStub.stubs.land.calledOnce).to.be.true;
         });
-        it('should then call bus.message');
-        it('should then call roundLogic.truncateBlocks if snapshotRound is true');
+
+        it('should then call bus.message', async () => {
+          await doCall();
+          expect(busStub.stubs.message.calledOnce).to.be.true;
+          expect(busStub.stubs.message.firstCall.args[0]).to.be.equal('finishRound');
+          expect(busStub.stubs.message.firstCall.args[1]).to.be.equal(roundLogicScope.round);
+          expect(roundLogicStub.stubs.land.calledBefore(busStub.stubs.message)).to.be.true;
+        });
+
+        it('should then call roundLogic.truncateBlocks if snapshotRound is true', async () => {
+          getSnapshotRoundsStub.returns(roundLogicScope.round);
+          await doCall();
+          expect(roundLogicStub.stubs.truncateBlocks.calledOnce).to.be.true;
+          expect(busStub.stubs.message.calledBefore(roundLogicStub.stubs.truncateBlocks));
+          expect(roundLogicStub.stubs.land.calledBefore(roundLogicStub.stubs.truncateBlocks));
+        });
       });
+
       describe(' else', () => {
-        it('should return null');
+        beforeEach(() => {
+          roundLogicScope.finishRound = false;
+        });
+
+        it('should not call roundLogic.land, bus.message, roundLogic.truncateBlocks', async () => {
+          await doCall();
+          expect(roundLogicStub.stubs.land.notCalled).to.be.true;
+          expect(busStub.stubs.message.notCalled).to.be.true;
+          expect(roundLogicStub.stubs.truncateBlocks.notCalled).to.be.true;
+        });
       });
-      it('should then call roundLogic.markBlockId');
+
+      it('should then call roundLogic.markBlockId', async () => {
+        await doCall();
+        expect(roundLogicStub.stubs.markBlockId.calledOnce).to.be.true;
+        expect(roundLogicStub.stubs.mergeBlockGenerator.calledBefore(roundLogicStub.stubs.markBlockId)).to.be.true;
+      });
     });
-    describe('in scoped afterTxPromise', () => {
+
+    describe('in afterTxPromise', () => {
+      let taskStub: { batch: SinonStub, none: SinonStub };
+
       const doCall = async () => {
-        await instance.backwardTick(block, previousBlock);
-        afterTxPromiseScoped = afterTxPromise('task');
-        return txGeneratorScoped('task');
+        await instance.tick(block);
+        return afterTxPromise();
       };
 
-      it('should not call anything if (block.height + 1) % this.slots.delegates !== 0');
-      it('should logger.debug');
-      it('should call db.tx');
-      it('should call tx.batch');
-      it('should call tx.none 4 times');
-      it('should call logger.error if round tx fails');
-      it('should reject if round tx fails');
-      it('should call logger.trace');
+      beforeEach(() => {
+        // (block.height + 1) % this.slots.delegates === 0
+        block.height = 100;
+        taskStub     = {
+          batch: sandbox.stub(),
+          none : sandbox.stub(),
+        };
+        dbStub.stubs.tx.callsArgWith(0, taskStub);
+        dbStub.stubs.tx.resolves('TX DONE');
+      });
+
+      it('should not call anything if (block.height + 1) % this.slots.delegates !== 0', async () => {
+        block.height = 1;
+        await doCall();
+        expect(loggerStub.stubs.debug.notCalled).to.be.true;
+        expect(dbStub.stubs.tx.notCalled).to.be.true;
+        expect(taskStub.batch.notCalled).to.be.true;
+        expect(loggerStub.stubs.error.notCalled).to.be.true;
+        expect(loggerStub.stubs.trace.notCalled).to.be.true;
+      });
+
+      it('should call logger.debug', async () => {
+        await doCall();
+        expect(loggerStub.stubs.debug.calledOnce).to.be.true;
+        expect(loggerStub.stubs.debug.firstCall.args[0]).to.be.equal('Performing round snapshot...');
+      });
+
+      it('should call db.tx', async () => {
+        await doCall();
+        expect(dbStub.stubs.tx.calledOnce).to.be.true;
+        expect(dbStub.stubs.tx.firstCall.args[0]).to.be.a('function');
+      });
+
+      describe('in db.tx callback', () => {
+        beforeEach(() => {
+          taskStub.none.onCall(0).returns(0);
+          taskStub.none.onCall(1).returns(1);
+          taskStub.none.onCall(2).returns(2);
+          taskStub.none.onCall(3).returns(3);
+        });
+
+        it('should call task.batch', async () => {
+          await doCall();
+          expect(taskStub.batch.calledOnce).to.be.true;
+          expect(taskStub.batch.firstCall.args[0]).to.be.deep.equal([0, 1, 2, 3]);
+        });
+
+        it('should call task.none 4 times', async () => {
+          await doCall();
+          expect(taskStub.none.callCount).to.be.equal(4);
+          expect(taskStub.none.getCall(0).args[0]).to.be.deep.equal(sql.clearRoundSnapshot);
+          expect(taskStub.none.getCall(1).args[0]).to.be.deep.equal(sql.performRoundSnapshot);
+          expect(taskStub.none.getCall(2).args[0]).to.be.deep.equal(sql.clearVotesSnapshot);
+          expect(taskStub.none.getCall(3).args[0]).to.be.deep.equal(sql.performVotesSnapshot);
+        });
+      });
+
+      it('should reject and call logger.error if round tx fails', async () => {
+        const theError = new Error('test');
+        dbStub.stubs.tx.rejects(theError);
+        await expect(doCall()).to.be.rejectedWith(theError);
+        expect(loggerStub.stubs.error.calledOnce).to.be.true;
+        expect(loggerStub.stubs.error.firstCall.args[0]).to.be.equal('Round snapshot failed');
+        expect(loggerStub.stubs.error.firstCall.args[1]).to.be.deep.equal(theError);
+      });
+
+      it('should call logger.trace after db.tx', async () => {
+        await doCall();
+        expect(loggerStub.stubs.trace.calledOnce).to.be.true;
+        expect(loggerStub.stubs.trace.firstCall.args[0]).to.be.equal('Round snapshot done');
+        expect(dbStub.stubs.tx.calledBefore(loggerStub.stubs.trace)).to.be.true;
+      });
     });
   });
 
   describe('getSnapshotRounds', () => {
-    it('should call appStateLogic.get and return it');
+    it('should call appStateLogic.get and return it if not false/undefined', () => {
+      appStateStub.stubs.get.returns(1);
+      const retVal = (instance as any).getSnapshotRounds();
+      expect(appStateStub.stubs.get.calledOnce).to.be.true;
+      expect(appStateStub.stubs.get.firstCall.args[0]).to.be.equal('rounds.snapshot');
+      expect(retVal).to.be.equal(1);
+    });
+
+    it('should  return 0 if appStateLogic.get returns false/undefined', () => {
+      appStateStub.stubs.get.returns(false);
+      const retVal = (instance as any).getSnapshotRounds();
+      expect(retVal).to.be.equal(0);
+      appStateStub.stubs.get.returns(undefined);
+      const retVal2 = (instance as any).getSnapshotRounds();
+      expect(retVal2).to.be.equal(0);
+    });
   });
 
   describe('innerTick', () => {
-    it('should call roundsLogic.calcRound twice');
-    it('should call appStateLogic.set');
-    it('should call sumRound if finishRound');
-    it('should set roundFees: 0, roundRewards: [0], roundDelegates: [block.generatorPublicKey] if block.height = 1');
-    it('should call getOutsiders if finishRound');
-    it('should build roundLogicScope as expected');
-    it('should call txGenerator');
-    it('should call db.tx');
-    it('should call afterTxPromise');
-    it('should call appStateLogic.set');
+    let txGeneratorStub: SinonStub;
+    let afterTxPromiseStub: SinonStub;
+    let sumRoundStub: SinonStub;
+    let getOutsidersStub: SinonStub;
+    let roundSums;
+
+    beforeEach(() => {
+      txGeneratorStub    = sandbox.stub();
+      afterTxPromiseStub = sandbox.stub();
+      sumRoundStub       = sandbox.stub(instance as any, 'sumRound');
+      roundSums          = {
+        roundFees     : 0,
+        roundRewards  : [0],
+        roundDelegates: ['delegate1', 'delegate2'],
+      };
+      sumRoundStub.returns(roundSums);
+      getOutsidersStub = sandbox.stub(instance as any, 'getOutsiders');
+      getOutsidersStub.resolves([]);
+      innerTickStub.restore();
+      appStateStub.stubs.set.returns(void 0);
+      roundsLogicStub.stubs.calcRound.returns(1);
+      dbStub.stubs.tx.resolves();
+      block.height = 98;
+    });
+
+    it('should call roundsLogic.calcRound twice', async () => {
+      await (instance as any).innerTick(block, true, txGeneratorStub, afterTxPromiseStub);
+      expect(roundsLogicStub.stubs.calcRound.calledTwice).to.be.true;
+      expect(roundsLogicStub.stubs.calcRound.firstCall.args[0]).to.be.equal(block.height);
+      expect(roundsLogicStub.stubs.calcRound.secondCall.args[0]).to.be.equal(block.height + 1);
+    });
+
+    it('should call appStateLogic.set twice if all OK', async () => {
+      await (instance as any).innerTick(block, true, txGeneratorStub, afterTxPromiseStub);
+      expect(appStateStub.stubs.set.calledTwice).to.be.true;
+      expect(appStateStub.stubs.set.firstCall.args[0]).to.be.equal('rounds.isTicking');
+      expect(appStateStub.stubs.set.firstCall.args[1]).to.be.true;
+      expect(appStateStub.stubs.set.secondCall.args[0]).to.be.equal('rounds.isTicking');
+      expect(appStateStub.stubs.set.secondCall.args[1]).to.be.false;
+    });
+
+    it('should call sumRound if block.height is 1', async () => {
+      block.height = 1;
+      await (instance as any).innerTick(block, true, txGeneratorStub, afterTxPromiseStub);
+      expect(sumRoundStub.calledOnce).to.be.true;
+      expect(sumRoundStub.firstCall.args[0]).to.be.equal(1);
+    });
+
+    it('should call sumRound if nextRound !== round', async () => {
+      roundsLogicStub.stubs.calcRound.onCall(0).returns(1);
+      roundsLogicStub.stubs.calcRound.onCall(1).returns(2);
+      await (instance as any).innerTick(block, true, txGeneratorStub, afterTxPromiseStub);
+      expect(sumRoundStub.calledOnce).to.be.true;
+      expect(sumRoundStub.firstCall.args[0]).to.be.equal(1);
+    });
+
+    it('should call txGenerator', async () => {
+      await (instance as any).innerTick(block, true, txGeneratorStub, afterTxPromiseStub);
+      expect(txGeneratorStub.calledOnce).to.be.true;
+      expect(txGeneratorStub.firstCall.args[0]).to.be.deep.equal({
+        backwards     : true,
+        block,
+        finishRound   : false,
+        library       : {
+          logger: loggerStub,
+        },
+        modules       : {
+          accounts: accountsModuleStub,
+        },
+        round         : 1,
+        roundOutsiders: null,
+      });
+    });
+
+    it('should set roundFees: 0, roundRewards: [0], roundDelegates: [generatorPublicKey] if height = 1', async () => {
+      block.height = 1;
+      await (instance as any).innerTick(block, true, txGeneratorStub, afterTxPromiseStub);
+      const generatedRoundLogicScope = txGeneratorStub.firstCall.args[0];
+      expect(generatedRoundLogicScope.roundFees).to.be.equal(0);
+      expect(generatedRoundLogicScope.roundRewards).to.be.deep.equal([0]);
+      expect(generatedRoundLogicScope.roundDelegates).to.be.deep.equal([block.generatorPublicKey]);
+    });
+
+    it('should call getOutsiders if finishRound', async () => {
+      // force finishRound
+      roundsLogicStub.stubs.calcRound.onCall(0).returns(1);
+      roundsLogicStub.stubs.calcRound.onCall(1).returns(2);
+      await (instance as any).innerTick(block, true, txGeneratorStub, afterTxPromiseStub);
+      expect(getOutsidersStub.calledOnce).to.be.true;
+      expect(getOutsidersStub.firstCall.args[0]).to.be.equal(1);
+      expect(getOutsidersStub.firstCall.args[1]).to.be.deep.equal(roundSums.roundDelegates);
+    });
+
+    it('should build roundLogicScope as expected', async () => {
+      // block height != 1, finishRound = false
+      const backwards = true;
+      await (instance as any).innerTick(block, backwards, txGeneratorStub, afterTxPromiseStub);
+      let generatedRoundLogicScope = txGeneratorStub.firstCall.args[0];
+      expect(generatedRoundLogicScope).to.be.deep.equal({
+        backwards,
+        block,
+        finishRound   : false,
+        library       : {
+          logger: loggerStub,
+        },
+        modules       : {
+          accounts: accountsModuleStub,
+        },
+        round         : 1,
+        roundOutsiders: null,
+      });
+
+      // block height != 1, finishRound = true
+      roundsLogicStub.reset();
+      txGeneratorStub.reset();
+      roundsLogicStub.stubs.calcRound.onCall(0).returns(1);
+      roundsLogicStub.stubs.calcRound.onCall(1).returns(2);
+      await (instance as any).innerTick(block, backwards, txGeneratorStub, afterTxPromiseStub);
+      generatedRoundLogicScope = txGeneratorStub.firstCall.args[0];
+      expect(generatedRoundLogicScope).to.be.deep.equal({
+        backwards,
+        block,
+        finishRound   : true,
+        library       : {
+          logger: loggerStub,
+        },
+        modules       : {
+          accounts: accountsModuleStub,
+        },
+        round         : 1,
+        roundOutsiders: [],
+        ...roundSums,
+      });
+
+      // block height = 1
+      roundsLogicStub.reset();
+      txGeneratorStub.reset();
+      roundsLogicStub.stubs.calcRound.returns(1);
+      block.height = 1;
+      await (instance as any).innerTick(block, backwards, txGeneratorStub, afterTxPromiseStub);
+      generatedRoundLogicScope = txGeneratorStub.firstCall.args[0];
+      expect(generatedRoundLogicScope).to.be.deep.equal({
+        backwards,
+        block,
+        finishRound   : true,
+        library       : {
+          logger: loggerStub,
+        },
+        modules       : {
+          accounts: accountsModuleStub,
+        },
+        round         : 1,
+        roundOutsiders: [],
+        roundFees     : 0,
+        roundRewards  : [0],
+        roundDelegates: [block.generatorPublicKey],
+      });
+
+    });
+
+    it('should call db.tx', async () => {
+      txGeneratorStub.returns('tx');
+      await (instance as any).innerTick(block, true, txGeneratorStub, afterTxPromiseStub);
+      expect(dbStub.stubs.tx.calledOnce).to.be.true;
+      expect(dbStub.stubs.tx.firstCall.args[0]).to.be.equal('tx');
+    });
+
+    it('should call afterTxPromise', async () => {
+      await (instance as any).innerTick(block, true, txGeneratorStub, afterTxPromiseStub);
+      expect(afterTxPromiseStub.calledOnce).to.be.true;
+      expect(dbStub.stubs.tx.calledBefore(afterTxPromiseStub)).to.be.true;
+    });
+
     describe('anything trows in main try block', () => {
-      it('should call logger.warn');
-      it('should call appStateLogic.set');
-      it('should throw the catched error');
+      it('should call logger.warn, call appStateLogic.set and throw the catched error', async () => {
+        const theError = new Error('test');
+        const backwards = true;
+        dbStub.stubs.tx.rejects(theError);
+        await expect((instance as any).innerTick(block, backwards, txGeneratorStub, afterTxPromiseStub)).to.be
+          .rejectedWith(theError);
+        expect(loggerStub.stubs.warn.calledOnce).to.be.true;
+        expect(loggerStub.stubs.warn.firstCall.args[0]).to.be
+          .equal(`Error while doing modules.innerTick [backwards=${backwards}]`);
+        expect(loggerStub.stubs.warn.firstCall.args[1]).to.be.equal(theError.message);
+        expect(appStateStub.stubs.set.calledTwice).to.be.true;
+        expect(appStateStub.stubs.set.secondCall.args[0]).to.be.equal('rounds.isTicking');
+        expect(appStateStub.stubs.set.secondCall.args[1]).to.be.false;
+      });
     });
   });
 
