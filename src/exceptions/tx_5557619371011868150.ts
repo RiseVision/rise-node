6f69fcdbdf6c4f31d1c55948c65f1c2d3cb2c3c7
Andrea Baccega <vekexasia@gmail.com>
2018-03-18 10:21:00 +0100

    Added Exception for transaction with wrong amount.

---
+++ src/exceptions/tx_5557619371011868150.ts
@@ -0,0 +1,28 @@
+import { ExceptionsList, ExceptionsManager, IExceptionHandler } from '../helpers';
+import { ITransactionLogic } from '../ioc/interfaces/logic';
+import { MemAccountsData } from '../logic/';
+import { IBaseTransaction} from '../logic/transactions';
+/**
+ * Failed to apply transaction 5557619371011868150 - Account does not have enough currency: 15844723232461775384R
+ * balance: 312.8466538 - 313.07107077 (0.23)
+ *
+ * Affected block was: 651118
+ */
+export default function exceptionTx5557619371011868150(excManager: ExceptionsManager) {
+  const handler: IExceptionHandler<ITransactionLogic> = {
+    canHandle(obj: ITransactionLogic, amount: number, balanceKey: 'balance'|'u_balance', tx: IBaseTransaction<void>) {
+      return tx.id === '5557619371011868150' &&
+        tx.senderPublicKey === '0275d0ee6f100cd429bbdc8556e3d1f49cca610f093c2e51e02cf038e8813282' &&
+        // tslint:disable-next-line
+        tx.signature === '74a014e909a532fc07f98ec4afe5f353ef274ac0bcda71308f06acbf434d60b0ff197eb9bb01b4360d34cbbb2e292842c6a6f7792089f58c2f5ea8578a10eb0c';
+    },
+    handle() {
+      return { error: false, exceeded: false };
+    },
+  };
+  excManager.registerExceptionHandler(
+    ExceptionsList.checkBalance,
+    'tx_5557619371011868150',
+    handler
+  );
+}
