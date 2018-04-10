f7de90365daf7a70dccf26588892827a0fc768d0
Andrea Baccega <vekexasia@gmail.com>
2018-04-10 21:42:52 +0200

    fix constants for testnet

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -54,7 +54,7 @@ export default {
   epochTime                    : new Date(Date.UTC(2016, 4, 24, 17, 0, 0, 0)),
   minVersion                   : [
     { height: 1, ver: '^0.1.0' },
-    { height: 500000,  ver: '>=0.1.2' },
+    { height: 500000,  ver: '>=0.1.0' },
   ],
   fees                         : [
     {
