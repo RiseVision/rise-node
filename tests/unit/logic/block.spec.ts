6a042fd557413b99b5e317527767183712586f7f
Andrea Baccega <vekexasia@gmail.com>
2018-05-20 18:10:00 +0200

    fixed block.spec.ts for new sequelize branch

--- tests/unit/logic/block.spec.ts
+++ tests/unit/logic/block.spec.ts
@@ -11,6 +11,8 @@ import {Symbols} from '../../../src/ioc/symbols';
 import { BlockLogic } from '../../../src/logic';
 import { BlockRewardLogicStub, TransactionLogicStub, ZSchemaStub } from '../../stubs';
 import { createContainer } from '../../utils/containerCreator';
+import { BlocksModel } from '../../../src/models';
+import { IBlockLogic } from '../../../src/ioc/interfaces/index';
 
 // tslint:disable-next-line no-var-requires
 const assertArrays = require('chai-arrays');
@@ -32,12 +34,13 @@ describe('logic/block', () => {
   let dummyBlock;
   let dummyTransactions;
   let callback;
-  let instance;
+  let instance: IBlockLogic;
   let data;
   let blockRewardLogicStub: BlockRewardLogicStub;
   let zschemastub: ZSchemaStub;
   let transactionLogicStub: TransactionLogicStub;
   let createHashSpy: SinonSpy;
+  let blocksModel: typeof BlocksModel;
 
   const bb = new ByteBuffer(1 + 4 + 32 + 32 + 8 + 8 + 64 + 64, true);
   bb.writeInt(123);
@@ -55,9 +58,9 @@ describe('logic/block', () => {
         id: '8139741256612355994',
         recipientId: '15256762582730568272R',
         senderId: '14709573872795067383R',
-        senderPublicKey: '35526f8a1e2f482264e5d4982fc07e73f4ab9f4794b110ceefecd8f880d51892',
-        signature: 'f8fbf9b8433bf1bbea971dc8b14c6772d33c7dd285d84c5e6c984b10c4141e9fa56ace' +
-        '902b910e05e98b55898d982b3d5b9bf8bd897083a7d1ca1d5028703e03',
+        senderPublicKey: Buffer.from('35526f8a1e2f482264e5d4982fc07e73f4ab9f4794b110ceefecd8f880d51892', 'hex'),
+        signature: Buffer.from('f8fbf9b8433bf1bbea971dc8b14c6772d33c7dd285d84c5e6c984b10c4141e9fa56ace' +
+        '902b910e05e98b55898d982b3d5b9bf8bd897083a7d1ca1d5028703e03', 'hex'),
         timestamp: 0,
         type: 1,
       },
@@ -67,9 +70,9 @@ describe('logic/block', () => {
         id: '16622990339377112127',
         recipientId: '6781920633453960895R',
         senderId: '14709573872795067383R',
-        senderPublicKey: '35526f8a1e2f482264e5d4982fc07e73f4ab9f4794b110ceefecd8f880d51892',
-        signature: 'e26edb739d93bb415af72f1c288b06560c0111c4505f11076ca20e2f6e8903d3b00730' +
-        '9c0e04362bfeb8bf2021d0e67ce3c943bfe0c0193f6c9503eb6dfe750c',
+        senderPublicKey: Buffer.from('35526f8a1e2f482264e5d4982fc07e73f4ab9f4794b110ceefecd8f880d51892', 'hex'),
+        signature: Buffer.from('e26edb739d93bb415af72f1c288b06560c0111c4505f11076ca20e2f6e8903d3b00730' +
+        '9c0e04362bfeb8bf2021d0e67ce3c943bfe0c0193f6c9503eb6dfe750c', 'hex'),
         timestamp: 0,
         type: 3,
       },
@@ -79,20 +82,20 @@ describe('logic/block', () => {
         id: '16622990339377114578',
         recipientId: '6781920633453960895R',
         senderId: '14709573872795067383R',
-        senderPublicKey: '35526f8a1e2f482264e5d4982fc07e73f4ab9f4794b110ceefecd8f880d51892',
-        signature: 'e26edb739d93bb415af72f1c288b06560c0111c4505f11076ca20e2f6e8903d3b00730' +
-        '9c0e04362bfeb8bf2021d0e67ce3c943bfe0c0193f6c9503eb6dfe750c',
+        senderPublicKey: Buffer.from('35526f8a1e2f482264e5d4982fc07e73f4ab9f4794b110ceefecd8f880d51892', 'hex'),
+        signature: Buffer.from('e26edb739d93bb415af72f1c288b06560c0111c4505f11076ca20e2f6e8903d3b00730' +
+        '9c0e04362bfeb8bf2021d0e67ce3c943bfe0c0193f6c9503eb6dfe750c', 'hex'),
         timestamp: 0,
         type: 2,
       },
     ];
 
     dummyBlock = {
-      blockSignature: '8c5f2b088eaf0634e1f6e12f94a1f3e871f21194489c76ad2aae5c1b71acd848bc7b' +
-      '158fa3b827e97f3f685c772bfe1a72d59975cbd2ccaa0467026d13bae50a',
-      generatorPublicKey: 'c950f1e6c91485d2e6932fbd689bba636f73970557fe644cd901a438f74883c5',
+      blockSignature: Buffer.from('8c5f2b088eaf0634e1f6e12f94a1f3e871f21194489c76ad2aae5c1b71acd848bc7b' +
+      '158fa3b827e97f3f685c772bfe1a72d59975cbd2ccaa0467026d13bae50a', 'hex'),
+      generatorPublicKey: Buffer.from('c950f1e6c91485d2e6932fbd689bba636f73970557fe644cd901a438f74883c5', 'hex'),
       numberOfTransactions: 2,
-      payloadHash: 'b3cf5bb113442c9ba61ed0a485159b767ca181dd447f5a3d93e9dd73564ae762',
+      payloadHash: Buffer.from('b3cf5bb113442c9ba61ed0a485159b767ca181dd447f5a3d93e9dd73564ae762', 'hex'),
       payloadLength: 8,
       previousBlock: '1',
       reward: 30000000,
@@ -106,14 +109,10 @@ describe('logic/block', () => {
     callback = sandbox.spy();
     zschemastub = container.get(Symbols.generic.zschema);
     blockRewardLogicStub = container.get(Symbols.logic.blockReward);
-    instance = new BlockLogic();
+    container.rebind(Symbols.helpers.ed).toConstantValue(ed);
+    container.rebind(Symbols.logic.block).to(BlockLogic).inSingletonScope();
+    instance = container.get(Symbols.logic.block);
     transactionLogicStub = container.get(Symbols.logic.transaction);
-
-    // Inject dependencies!
-    (instance as any).zschema = zschemastub;
-    (instance as any).blockReward = blockRewardLogicStub;
-    (instance as any).ed = ed;
-    (instance as any).transaction = transactionLogicStub;
 
     // Default stub configuration
     blockRewardLogicStub.stubConfig.calcReward.return = 100000;
@@ -126,6 +125,8 @@ describe('logic/block', () => {
       timestamp: Date.now(),
       transactions: dummyTransactions,
     };
+
+    blocksModel = container.get(Symbols.models.blocks);
   });
 
   afterEach(() => {
@@ -174,7 +175,7 @@ describe('logic/block', () => {
   describe('sign', () => {
     it('should return a block signature with 128 of length', () => {
       const blockSignature = instance.sign(dummyBlock, dummyKeypair);
-      expect(blockSignature).to.have.lengthOf(128);
+      expect(blockSignature).to.have.lengthOf(64);
     });
 
     it('should call ed.sign', () => {
@@ -236,47 +237,12 @@ describe('logic/block', () => {
   describe('dbSave', () => {
     it('should return a specific object', () => {
       const result = instance.dbSave(dummyBlock);
-      expect(result).to.be.an.instanceof(Object);
-      expect(result.table).to.equal('blocks');
-      expect(result.fields).to.be.equalTo([
-        'id',
-        'version',
-        'timestamp',
-        'height',
-        'previousBlock',
-        'numberOfTransactions',
-        'totalAmount',
-        'totalFee',
-        'reward',
-        'payloadLength',
-        'payloadHash',
-        'generatorPublicKey',
-        'blockSignature',
-      ]);
-      expect(result.values).to.have.all.keys([
-        'id',
-        'version',
-        'timestamp',
-        'height',
-        'previousBlock',
-        'numberOfTransactions',
-        'totalAmount',
-        'totalFee',
-        'reward',
-        'payloadLength',
-        'payloadHash',
-        'generatorPublicKey',
-        'blockSignature',
-      ]);
-    });
-  });
+      expect(result.model).to.be.deep.eq(blocksModel);
+      expect(result.type).to.be.deep.eq('create');
+      const toSave = {... dummyBlock};
+      delete toSave.transactions;
+      expect(result.values).to.be.deep.eq(toSave);
 
-  describe('dbSave() with wrong parameters', () => {
-    it('should throw an exception if invalid object is passed', () => {
-      const wrongBlock = {};
-      expect(() => {
-        instance.dbSave(wrongBlock);
-      }).to.throw();
     });
   });
 
@@ -337,13 +303,13 @@ describe('logic/block', () => {
 
   describe('dbRead', () => {
     const raw = {
-      b_blockSignature: 16,
+      b_blockSignature: Buffer.alloc(64).fill('a'),
+      b_payloadHash: Buffer.alloc(32).fill('a'),
       b_confirmations: 1,
       b_generatorPublicKey: 'c950f1e6c91485d2e6932fbd689bba636f73970557fe644cd901a438f74883c5',
       b_height: 12,
       b_id: 10,
       b_numberOfTransactions: 1,
-      b_payloadHash: 14,
       b_payloadLength: 13,
       b_previousBlock: 9,
       b_reward: 50,
@@ -371,7 +337,6 @@ describe('logic/block', () => {
         'generatorPublicKey',
         'generatorId',
         'blockSignature',
-        'confirmations',
         'totalForged',
       ]);
       expect(block.totalForged).to.equal('150');
@@ -387,10 +352,10 @@ describe('logic/block', () => {
       getAddressByPublicKeySpy.restore();
     });
 
-    it('should call parseInt on 9 fields', () => {
+    it('should call parseInt on 8 fields', () => {
       const parseIntSpy = sinon.spy(global, 'parseInt');
       instance.dbRead(raw);
-      expect(parseIntSpy.callCount).to.be.eq(9);
+      expect(parseIntSpy.callCount).to.be.eq(8);
       parseIntSpy.restore();
     });
     it('should return null is raw.d_id is undefined', () => {
@@ -398,4 +363,5 @@ describe('logic/block', () => {
       expect(instance.dbRead(raw)).to.be.null;
     });
   });
+
 });
