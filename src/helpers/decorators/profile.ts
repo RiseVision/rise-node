42bc4b0b62fc7a156a64c893b8293feb7ab7cfa4
Andrea Baccega <vekexasia@gmail.com>
2018-06-03 19:02:45 +0200

    added profile decorator for non async functions.

--- src/helpers/decorators/profile.ts
+++ src/helpers/decorators/profile.ts
@@ -25,3 +25,30 @@ export function profilePromise(snap: number  = 100) {
     };
   };
 }
+
+export function profile(snap: number = 100) {
+  return (target: any,
+          method: string,
+          descriptor: TypedPropertyDescriptor<(...args: any[]) => any>) => {
+    const oldValue     = descriptor.value;
+    let totalElapsed = 0;
+    let count = 0;
+    let lastMark = 0;
+    descriptor.value   = function profileWrapper(...args: any[]) {
+      const pre = Date.now();
+      const res = oldValue.apply(this, args);
+      const after = Date.now();
+      totalElapsed += after - pre;
+      count ++;
+      lastMark += after - pre;
+      if (count % snap === 0) {
+        const elapsedFromLast = lastMark / snap;
+        lastMark = 0;
+        fs.appendFileSync(
+          `${__dirname}/../../../prof_${target.constructor.name}_${method}.txt`,
+          `${count} - ${totalElapsed / count} - ${elapsedFromLast / snap} \n`);
+      }
+      return res;
+    };
+  };
+}
