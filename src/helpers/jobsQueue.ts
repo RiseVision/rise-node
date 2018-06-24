5e04987afe307901a8715917daf23060bd9bd2bf
Jose Antonio <jguidos@hotmail.com>
2018-06-24 10:56:33 +0200

    Added comments to helpers/jobsQueue

--- src/helpers/jobsQueue.ts
+++ src/helpers/jobsQueue.ts
@@ -1,10 +1,16 @@
 import { injectable } from 'inversify';
 import { IJobsQueue } from '../ioc/interfaces/helpers';
 
+/**
+ * Contains a queue of jobs
+ */
 @injectable()
 export class JobsQueue implements IJobsQueue {
   public jobs: { [k: string]: NodeJS.Timer } = {};
 
+  /**
+   * Add a new job to the queue
+   */
   public register(name: string, job: () => Promise<any>, time: number) {
     if (this.jobs[name]) {
       throw new Error('Synchronous job ' + name + ' already registered');
