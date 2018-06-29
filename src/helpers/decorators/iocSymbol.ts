3a656a7a2df12e7c9124114d77d07c14930c7f5a
Jose Antonio <jguidos@hotmail.com>
2018-06-29 09:52:03 +0200

    Added comments to helpers/decorators/iocSymbol

--- src/helpers/decorators/iocSymbol.ts
+++ src/helpers/decorators/iocSymbol.ts
@@ -1,5 +1,9 @@
 import 'reflect-metadata';
 import { Symbols } from '../../ioc/symbols';
+
+/**
+ * Decorator for to define class Symbol metadata
+ */
 export function IoCSymbol(symbol: symbol): ClassDecorator {
   // tslint:disable-next-line ban-types
   return function iocDecorator<T extends Function>(t: T) {
