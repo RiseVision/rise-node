d88341d0c00b3f5cb34bf9a4a68affbdc1df228c
Jose Antonio <jguidos@hotmail.com>
2018-07-02 10:38:26 +0200

    Added comments for ioc/interfaces/helpers/IJobsQueue

--- src/ioc/interfaces/helpers/IJobsQueue.ts
+++ src/ioc/interfaces/helpers/IJobsQueue.ts
@@ -1,5 +1,8 @@
 export interface IJobsQueue {
   jobs: { [k: string]: NodeJS.Timer };
 
+  /**
+   * Add a new job to the queue
+   */
   register(name: string, job: () => Promise<any>, time: number): NodeJS.Timer;
 }
