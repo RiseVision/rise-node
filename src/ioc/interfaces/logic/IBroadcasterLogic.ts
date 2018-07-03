d04f6fed0e3abbd13a33a37db7e8d628cb94756c
Jose Antonio <jguidos@hotmail.com>
2018-07-03 09:04:00 +0200

    Addded comments to /ioc/interfaces/logic/IBroadcasterLogic

--- src/ioc/interfaces/logic/IBroadcasterLogic.ts
+++ src/ioc/interfaces/logic/IBroadcasterLogic.ts
@@ -2,10 +2,19 @@ import { BroadcastTaskOptions, PeerType } from '../../../logic';
 
 export interface IBroadcasterLogic {
 
+  /**
+   * Get peers
+   */
   getPeers(params: { limit?: number, broadhash?: string }): Promise<PeerType[]>;
 
+  /**
+   * Enqueue a BroadcastTask
+   */
   enqueue(params: any, options: BroadcastTaskOptions): number;
 
+  /**
+   * Broadcast to peers
+   */
   broadcast(params: {
               limit?: number, broadhash?: string,
               peers?: PeerType[]
