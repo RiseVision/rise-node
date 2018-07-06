91c77c8c4f31be8407d6ff5bcf66a359610d50dc
Jose Antonio <jguidos@hotmail.com>
2018-07-06 11:38:20 +0200

    Added comments for /ioc/interfaces/logic/IPeersLogic

--- src/ioc/interfaces/logic/IPeersLogic.ts
+++ src/ioc/interfaces/logic/IPeersLogic.ts
@@ -1,7 +1,14 @@
 import { BasePeerType, PeerType } from '../../../logic';
 import { IPeerLogic } from './IPeerLogic';
 
+/**
+ * Methods signature for a peer
+ */
 export interface IPeersLogic {
+
+  /**
+   * Creates a returns a PeerLogic instance
+   */
   create(peer: BasePeerType): IPeerLogic;
 
   /**
@@ -9,12 +16,25 @@ export interface IPeersLogic {
    */
   exists(peer: BasePeerType): boolean;
 
+  /**
+   * Returns a PeerLogic instance from a PeerType or alias
+   */
   get(peer: PeerType | string): IPeerLogic;
 
+  /**
+   * Add or replace a peer into peers list
+   */
   upsert(peer: PeerType, insertOnly: boolean): boolean;
 
+  /**
+   * Remove a peer from peers list
+   */
   remove(peer: BasePeerType): boolean;
 
+  /**
+   * Returns peers list. If normalized is true you will get a list of PeerType peers,
+   * otherwise you will get a list of PeerLogic peers.
+   */
   list(normalize: true): PeerType[];
 
   list(normalize: false): IPeerLogic[];
