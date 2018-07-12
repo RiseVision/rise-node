79eb1aaf03d425445b4daf00d056024d643a8432
Jose Antonio <jguidos@hotmail.com>
2018-07-12 11:59:32 +0200

    Added comments to /ioc/interfaces/modules/IForkModule

--- src/ioc/interfaces/modules/IForkModule.ts
+++ src/ioc/interfaces/modules/IForkModule.ts
@@ -1,6 +1,9 @@
 import { ForkType } from '../../../helpers';
 import { SignedBlockType } from '../../../logic';
 
+/**
+ * Methods signature for ForkModule
+ */
 export interface IForkModule {
   /**
    * Inserts a fork into fork_stats table and emits a socket signal with the fork data
