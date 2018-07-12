5b80fb110f5806b7ebbc4099fc26e0a8a689dcf9
Jose Antonio <jguidos@hotmail.com>
2018-07-12 11:58:27 +0200

    Added comments to /ioc/interfaces/modules/ICacheModule

--- src/ioc/interfaces/modules/ICacheModule.ts
+++ src/ioc/interfaces/modules/ICacheModule.ts
@@ -1,23 +1,50 @@
 import { RedisClient } from 'redis';
 import { IModule } from './IModule';
 
+/**
+ * Methods signature for Cache
+ */
 export interface ICacheModule extends IModule {
   readonly isConnected: boolean | RedisClient | boolean;
 
+  /**
+   * Resolves if is connected, otherwise rejects
+   */
   assertConnected(): Promise<void>;
 
+  /**
+   * Resolves if cache is ready and connected, otherwise rejects
+   */
   assertConnectedAndReady(): Promise<void>;
 
+  /**
+   * Gets a value from a given key
+   */
   getObjFromKey<T>(k: string): Promise<T>;
 
+  /**
+   * Stores a key/value pair
+   */
   setObjFromKey<T>(k: string, value: any): Promise<void>;
 
+  /**
+   * Removes values from given keys
+   */
   deleteJsonForKey(k: string | string[]): Promise<void>;
 
+  /**
+   * Removes values using a regular expression
+   */
   removeByPattern(pattern: string): Promise<void>;
 
+  /**
+   * Delete all the keys
+   */
   flushDb(): Promise<void>;
 
+  /**
+   * Close connection
+   */
   quit(): Promise<void>;
 
 }
