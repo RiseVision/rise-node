fb060ff5318c3612355fb27874806dae557e3376
Jose Antonio <jguidos@hotmail.com>
2018-07-13 11:53:56 +0200

    Added comments to /ioc/interfaces/modules/IMultisignaturesModule

--- src/ioc/interfaces/modules/IMultisignaturesModule.ts
+++ src/ioc/interfaces/modules/IMultisignaturesModule.ts
@@ -1,8 +1,10 @@
+/**
+ * Methods signature for MultisignaturesModule
+ */
 export interface IMultisignaturesModule {
+
   /**
-   * Gets the tx from the txID, verifies the given signature and
-   * @param {{signature: any; transaction: string}} tx
-   * @return {Promise<void>}
+   * Gets the tx from the txID, verifies the given signature and transaction
    */
   processSignature(tx: { signature: any, transaction: string }): Promise<void>;
 }
