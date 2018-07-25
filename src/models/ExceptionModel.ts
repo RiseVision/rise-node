832b2add1f89c2239ebd576bf0fa1a3067c40e17
Jose Antonio <jguidos@hotmail.com>
2018-07-25 14:31:02 +0200

    Added comments to /models/ExceptionModel

--- src/models/ExceptionModel.ts
+++ src/models/ExceptionModel.ts
@@ -1,6 +1,9 @@
 import { Column, Model, PrimaryKey, Table } from 'sequelize-typescript';
 
 @Table({ tableName: 'exceptions' })
+/**
+ * Exception model
+ */
 export class ExceptionModel extends Model<ExceptionModel> {
   @PrimaryKey
   @Column
