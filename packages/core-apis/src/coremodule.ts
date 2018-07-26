49c7cccc026cad01c6dbed0b0da896c5004cd1f3
Andrea Baccega <vekexasia@gmail.com>
2018-07-26 17:19:40 +0200

    fixed core-apis.

--- packages/core-apis/src/coremodule.ts
+++ packages/core-apis/src/coremodule.ts
@@ -1,8 +1,9 @@
 import { BaseCoreModule } from '@risevision/core-launchpad';
+import { p2pSymbols } from '@risevision/core-p2p';
 import { AppConfig } from '@risevision/core-types';
 import { APISymbols, limitsMiddleware } from './helpers';
 import { APIErrorHandler, PrivateApisGuard, SuccessInterceptor } from './utils';
-import { P2PSymbols } from '@risevision/core-p2p';
+import { Symbols } from '@risevision/core-interfaces';
 
 export class CoreModule extends BaseCoreModule<AppConfig> {
   public configSchema = require('../schema/config.json');
@@ -11,13 +12,18 @@ export class CoreModule extends BaseCoreModule<AppConfig> {
   // this.container.bind(Symbols.api.utils.forgingApisWatchGuard).to(ForgingApisWatchGuard).inSingletonScope();
   // this.container.bind(Symbols.api.utils.attachPeerHeaderToResponseObject).to(AttachPeerHeaders).inSingletonScope();
   public addElementsToContainer() {
-    this.container.bind(P2PSymbols.middleware).to(APIErrorHandler).inSingletonScope();
-    this.container.bind(APISymbols.successInterceptor).to(SuccessInterceptor).inSingletonScope();
-    this.container.bind(APISymbols.privateApiGuard).to(PrivateApisGuard).inSingletonScope();
-    this.container.bind(APISymbols.applyLimitsMiddleware).toConstantValue(limitsMiddleware);
+    this.container.bind(p2pSymbols.middleware).to(APIErrorHandler)
+      .inSingletonScope()
+      .whenTargetNamed(APISymbols.errorHandler);
+
+    this.container.bind(Symbols.class)
+      .to(SuccessInterceptor).inSingletonScope().whenTargetNamed(APISymbols.successInterceptor);
+    this.container
+      .bind(Symbols.class)
+      .to(PrivateApisGuard).inSingletonScope().whenTargetNamed(APISymbols.privateApiGuard);
+    this.container
+      .bind(Symbols.class)
+      .toConstantValue(limitsMiddleware).whenTargetNamed(APISymbols.applyLimitsMiddleware);
   }
 
-  public initAppElements() {
-
-  }
 }
