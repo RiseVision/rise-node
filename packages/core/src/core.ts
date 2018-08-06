1c1ddd7e2e00d596c5e2d253b800c6a6990700da
Andrea Baccega <vekexasia@gmail.com>
2018-08-06 11:17:01 +0200

    fix wrong code in core.

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -1,8 +1,8 @@
+import { APISymbols } from '@risevision/core-apis';
 import { BaseCoreModule } from '@risevision/core-launchpad';
 import { constants } from './constants';
 import { LoaderModule, SystemModule } from './modules';
 import { CoreSymbols } from './symbols';
-import { APISymbols } from '../../core-apis/src/helpers';
 
 export class CoreModule extends BaseCoreModule<void> {
   public configSchema = {};
@@ -12,8 +12,8 @@ export class CoreModule extends BaseCoreModule<void> {
     this.container.bind(CoreSymbols.modules.system).to(SystemModule).inSingletonScope();
     this.container.bind(CoreSymbols.modules.loader).to(LoaderModule).inSingletonScope();
     this.container.bind(APISymbols.api).to(LoaderModule)
-      .whenTargetNamed(CoreSymbols.api.loader)
-      .inSingletonScope();
+      .inSingletonScope()
+      .whenTargetNamed(CoreSymbols.api.loader);
   }
 
 }
