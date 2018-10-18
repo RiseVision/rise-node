6019a9f12c950c577e5f8a9ef1d728ba33029ea8
Andrea B <vekexasia@gmail.com>
2018-10-18 17:32:25 +0200

    :rotating_light: some new tests and edgecases on fairvote and highlevel.

--- tests/integration/functionalities/fairVote.spec.ts
+++ tests/integration/functionalities/fairVote.spec.ts
@@ -1,10 +1,14 @@
 import { expect } from 'chai';
 import {
-  addNewDelegate, createRandomAccountWithFunds,
-  createRegDelegateTransaction, createVoteTransaction, createWallet, removeDelegatePass,
+  addNewDelegate,
+  createRandomAccountWithFunds,
+  createRegDelegateTransaction,
+  createVoteTransaction,
+  createWallet,
+  removeDelegatePass,
 } from '../common/utils';
 import { LiskWallet } from 'dpos-offline/dist/es5/liskWallet';
-import { BlocksModule, RoundsModule } from '../../../src/modules';
+import { BlocksModule } from '../../../src/modules';
 import initializer from '../common/init';
 import { Symbols } from '../../../src/ioc/symbols';
 import { toBufferedTransaction } from '../../utils/txCrafter';
@@ -26,19 +30,19 @@ describe('Fair vote system', async () => {
   });
   initializer.setup();
   beforeEach(async () => {
-    blocksModule      = initializer.appManager.container.get(Symbols.modules.blocks);
+    blocksModule  = initializer.appManager.container.get(Symbols.modules.blocks);
     accountsModel = initializer.appManager.container.get<typeof AccountsModel>(Symbols.models.accounts);
     // Each Delegate has a genesis balance of 108910891000000 and voted for himself. We transfer 1 third of balance
     // of a random genesis delegate to a new account that will vote for himself. This will give enough funds to increase
     // the forging chances, but not with normal dpos algorithm.
-    const secret = uuid.v4();
+    const secret      = uuid.v4();
     newDelegateWallet = createWallet(secret);
     await createRandomAccountWithFunds(Math.floor(108910891000000 / 3), newDelegateWallet);
     addNewDelegate({
-      address: newDelegateWallet.address,
-      keypair: {
+      address : newDelegateWallet.address,
+      keypair : {
         privateKey: newDelegateWallet.privKey,
-        publicKey: newDelegateWallet.publicKey,
+        publicKey : newDelegateWallet.publicKey,
       },
       secret,
       username: 'outsider',
@@ -52,11 +56,11 @@ describe('Fair vote system', async () => {
     await initializer.rawDeleteBlocks(3);
   });
 
-  it ('should at some point include an outsider in round, when dposv2 is active', async function() {
+  it('should at some point include an outsider in round, when dposv2 is active', async function () {
     this.timeout(1000000);
     let minedBlocks = 0;
-    let found = false;
-    const acc = await accountsModel.findById(newDelegateWallet.address);
+    let found       = false;
+    const acc       = await accountsModel.findById(newDelegateWallet.address);
     // Make sure the bew delegate hasn't forget yet
     expect(blocksModule.lastBlock.height).to.be.lt(constants.activeDelegates);
     expect(acc).not.to.be.undefined;
@@ -82,13 +86,13 @@ describe('Fair vote system', async () => {
     expect(found).to.be.true;
   });
 
-  it ('should reduce votesWeight for delegates with productivity lower than 100%', async function() {
+  it('should reduce votesWeight for delegates with productivity lower than 100%', async function () {
     this.timeout(1000000);
-    let blocksToDelete = 0;
+    let blocksToDelete        = 0;
     const blocksToFinishRound = constants.activeDelegates - blocksModule.lastBlock.height;
     await initializer.rawMineBlocks(blocksToFinishRound);
     blocksToDelete += blocksToFinishRound;
-    const acc = await accountsModel.findById(newDelegateWallet.address);
+    const acc                = await accountsModel.findById(newDelegateWallet.address);
     const initialVotesWeight = acc.votesWeight;
     expect(initialVotesWeight).to.be.equal(acc.balance);
     acc.set('producedblocks', 200);
@@ -103,8 +107,30 @@ describe('Fair vote system', async () => {
     await initializer.rawDeleteBlocks(blocksToDelete);
     expect(accAfter.producedblocks).to.be.eq(201);
     expect(accAfter.missedblocks).to.be.eq(201);
-    expect(accAfter.votesWeight).to.be.gte(Math.floor(accAfter.balance * 0.5));
-    expect(accAfter.votesWeight).to.be.lt(Math.floor(accAfter.balance * 0.51));
+    expect(accAfter.votesWeight).to.be.eq(Math.floor(accAfter.balance * 0.5));
+  });
+  it('should work even with 66.666% productivity', async function() {
+    this.timeout(1000000);
+    let blocksToDelete        = 0;
+    const blocksToFinishRound = constants.activeDelegates - blocksModule.lastBlock.height;
+    await initializer.rawMineBlocks(blocksToFinishRound);
+    blocksToDelete += blocksToFinishRound;
+    const acc                = await accountsModel.findById(newDelegateWallet.address);
+    const initialVotesWeight = acc.votesWeight;
+    expect(initialVotesWeight).to.be.equal(acc.balance);
+    acc.set('producedblocks', 66666 - 1);
+    acc.set('missedblocks', 100000 - 66666);
+    await acc.save();
+    let accAfter = acc;
+    while (accAfter.producedblocks < 66666) {
+      await initializer.rawMineBlocks(constants.activeDelegates);
+      blocksToDelete += constants.activeDelegates;
+      accAfter = await accountsModel.findById(newDelegateWallet.address);
+    }
+    await initializer.rawDeleteBlocks(blocksToDelete);
+    expect(accAfter.producedblocks).to.be.eq(66666);
+    expect(accAfter.missedblocks).to.be.eq(100000 - 66666);
+    expect(accAfter.votesWeight).to.be.eq(Math.round(accAfter.balance * 0.66666));
   });
 
 });
