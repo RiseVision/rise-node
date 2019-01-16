e25a7c171be57955b3f806e70cc231eda954925d
Mart Roosmaa <mart@roosmaa.net>
2019-01-16 14:14:42 +0100

    Replace the watchpackagesrecompile.sh with transpile-watch.sh.

--- .devutils/watchpackagesrecompile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-inotifywait -q -m -e close_write --format "%w %e %f" -r $(ls packages | xargs -I {} echo packages/{}/src ) |
-while read events; do
-    (echo $events;
-    PACKAGE=$(echo $events | cut -d ' ' -f 1 | cut -d '/' -f2)
-    echo -n "Recompiling $PACKAGE ..."
-    (yarn single-transpile $PACKAGE >> /dev/null && echo ' ok') &) &
-done;
