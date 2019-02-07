0bf231b43cd6de10427eea9d06b657dd0d03c76f
Julian Popescu <jpopesculian@gmail.com>
2019-02-07 17:57:07 +0100

    sync package / readme info with root

---
+++ .devutils/update-package-info.ts
@@ -0,0 +1,116 @@
+import * as fs from 'fs';
+import * as _ from 'lodash/fp';
+import * as path from 'path';
+
+const MD_AUTHORS = 'Authors';
+const MD_LICENSE = 'License';
+
+const packageKeys = [
+  'homepage',
+  'bugs',
+  'license',
+  'author',
+  'contributors',
+  'repository',
+];
+
+const root = _.memoize((): string => path.join(__dirname, '../'));
+const absPath = (...relativePath: string[]): string =>
+  path.join(root(), ...relativePath);
+const allPackageNames = _.memoize(() =>
+  fs
+    .readdirSync('packages')
+    .map((d) => path.join(root(), 'packages', d))
+    .filter((d) => fs.lstatSync(d).isDirectory())
+    .map((d) => path.join('packages', path.basename(d)))
+);
+
+const read = (...relativePath: string[]): string =>
+  fs.readFileSync(absPath(...relativePath), 'utf8').toString();
+const write = (data: string, ...relativePath: string[]) =>
+  fs.writeFileSync(absPath(...relativePath), data, { flag: 'w+' });
+
+const testHeader = (name: string = '') => {
+  return (value: string) => {
+    return new RegExp('^#+ ' + name + '.*').test(value);
+  };
+};
+
+const findSection = (name: string, mdLines: string[]): number[] => {
+  const startIndex = _.findIndex(testHeader(name), mdLines);
+  if (startIndex < 0) {
+    return [-1, -1];
+  }
+  const endIndex = _.findIndexFrom(testHeader(), startIndex + 1, mdLines);
+  if (endIndex < 0) {
+    return [startIndex, mdLines.length];
+  }
+  return [startIndex, endIndex];
+};
+
+const getSection = (name: string, mdLines: string[]): string[] => {
+  const [start, end] = findSection(name, mdLines);
+  if (start < 0) {
+    return [];
+  }
+  return mdLines.slice(start, end);
+};
+
+const replaceSection = (
+  name: string,
+  mdLines: string[],
+  replacement: string[]
+) => {
+  const [start, end] = findSection(name, mdLines);
+  if (start < 0) {
+    mdLines.push('', ...replacement);
+  } else {
+    mdLines.splice(start, end - start - 1, ...replacement);
+  }
+  return mdLines;
+};
+
+const getReadmeInfo = (filepath: string) => {
+  const mdLines = read(filepath).split('\n');
+  return [getSection(MD_AUTHORS, mdLines), getSection(MD_LICENSE, mdLines)];
+};
+
+const getPackageInfo = (filepath: string) => {
+  const packageJson = JSON.parse(read(filepath));
+  return _.pick(packageKeys, packageJson);
+};
+
+const replaceReadmeInfo = (
+  filepath: string,
+  [authors, license]: string[][]
+) => {
+  let mdLines = read(filepath).split('\n');
+  mdLines = replaceSection(MD_AUTHORS, mdLines, authors);
+  mdLines = replaceSection(MD_LICENSE, mdLines, license);
+  write(mdLines.join('\n'), filepath);
+};
+
+const replacePackageInfo = (filepath: string, packageInfo: object) => {
+  const packageString = read(filepath);
+  const packageJson = _.merge(JSON.parse(packageString), packageInfo);
+  const space = packageString.split('\n')[1].match(/^\s+/)[0];
+  write(JSON.stringify(packageJson, null, space), filepath);
+};
+
+const updateReadmeInfos = () => {
+  const readmeInfo = getReadmeInfo('README.md');
+  const license = read('LICENSE');
+  const packageInfo = getPackageInfo('package.json');
+  for (const packageName of allPackageNames()) {
+    console.log(`Updating ${path.basename(packageName)}`);
+    replaceReadmeInfo(path.join(packageName, 'README.md'), readmeInfo);
+    replacePackageInfo(path.join(packageName, 'package.json'), packageInfo);
+    write(license, path.join(packageName, 'LICENSE'));
+  }
+};
+
+const main = () => {
+  updateReadmeInfos();
+};
+
+main();
---
+++ packages/rise/README.md
@@ -0,0 +1,47 @@
+# RISE Node Core: Migration Module
+
+
+
+## Authors
+- Andrea B. <vekexasia+crypto@gmail.com>
+- Jan <lepetitjan@icloud.com>
+- Mariusz Serek <mariusz@serek.net>
+- Goldeneye (Shift Team)
+- Ralfs (Shift Team)
+- Joey <shiftcurrency@gmail.com>
+- Boris Povod <boris@crypti.me>
+- Pavel Nekrasov <landgraf.paul@gmail.com>
+- Sebastian Stupurac <stupurac.sebastian@gmail.com>
+- Oliver Beddows <oliver@lightcurve.io>
+- Isabella Dell <isabella@lightcurve.io>
+- Marius Serek <mariusz@serek.net>
+- Maciej Baj <maciej@lightcurve.io>
+
+
+## License
+
+Copyright © 2017 Rise<br>
+Copyright © 2016-2017 Shift<br>  
+Copyright © 2016-2017 Lisk Foundation
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the [GNU General Public License](https://github.com/RiseVision/rise-node/src/master/LICENSE) along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+***
+
+This program also incorporates work previously released with lisk `0.7.0` (and earlier) versions under the [MIT License](https://opensource.org/licenses/MIT). To comply with the requirements of that license, the following permission notice, applicable to those parts of the code only, is included below:
+
+Copyright © 2017 Rise<br>
+Copyright © 2016-2017 Shift<br>
+Copyright © 2016-2017 Lisk Foundation<br>  
+Copyright © 2015 Crypti
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
