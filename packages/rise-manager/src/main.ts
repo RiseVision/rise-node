2fcaffd0acd5dd04c062b34c4d435b007af47624
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2019-05-31 22:58:32 +0200

    simplified child_process `rise-manager node start` works

--- packages/rise-manager/src/main.ts
+++ packages/rise-manager/src/main.ts
@@ -1,10 +1,11 @@
 import { branch, cli, leaf, option } from '@carnesen/cli';
-import * as execa from 'execa';
+import { exec } from 'child_process';
+import * as debug from 'debug';
 import { resolve } from 'path';
-import { Writable } from 'stream';
 
 const SEC = 1000;
 const MIN = 60 * SEC;
+const log = debug('manager');
 
 // ----- COMMANDS
 
@@ -17,55 +18,90 @@ export const node_start = leaf({
       typeName: 'string',
       nullable: true,
       defaultValue: 'config.json',
+      description: 'Path to the config file',
     }),
     network: option({
       typeName: 'string',
       nullable: true,
       defaultValue: 'mainnet',
-      // TODO limit values
+      allowedValues: ['mainnet', 'testnet'],
+    }),
+    foreground: option({
+      typeName: 'boolean',
+      nullable: true,
+      defaultValue: false,
+      description: 'Keep the process in the foreground. Implies --showLog',
+    }),
+    show_log: option({
+      typeName: 'boolean',
+      nullable: true,
+      defaultValue: false,
+      description: 'Stream the console output',
     }),
   },
 
-  async action({ config, network }) {
+  async action({ config, network, foreground, show_log }) {
     // TODO check if in the correct dir
     // TODO check if `./node_modules/.bin/lerna` exists
+    const showLog = show_log || foreground;
     const config_path = resolve(config);
     console.log('Stating RISE node...');
+    let ready = false;
     try {
-      const lerna_run = new Promise((resolve, reject) => {
+      await new Promise((resolve, reject) => {
         // kill the process after 2 mins
         const timeout = setTimeout(reject.bind(null, 'TIMEOUT'), 2 * MIN);
-        // parse the commands output
-        const stream = new Writable({
-          write(chunk, encoding, callback) {
-            // decode
-            chunk = chunk.toString('utf8');
-            console.log('chunk', chunk);
-            // check if the output reached the desired line
-            if (chunk.includes('Blockchain ready')) {
-              clearTimeout(timeout);
+
+        const cmd = exec(
+          './node_modules/.bin/lerna run ' +
+            `start:${network} ` +
+            '--stream ' +
+            '--no-prefix ' +
+            '-- ' +
+            `-e ${config_path}`
+        );
+
+        cmd.stdout.on('data', (data) => {
+          // output
+          if (showLog) {
+            process.stdout.write(data);
+          } else {
+            data = data.toString('utf8');
+            log(data);
+          }
+          // check if the output reached the desired line
+          if (data.includes('Blockchain ready')) {
+            clearTimeout(timeout);
+            ready = true;
+            // keep streaming the output if in foreground
+            if (!foreground) {
               resolve();
             }
-            // mark the chunk as written
-            callback();
-          },
+          }
         });
 
-        const cmd = execa(`./node_modules/.bin/lerna`, [
-          'run',
-          `start:${network}`,
-          `--stream`,
-          `--no-prefix`,
-          `--`,
-          `-e ${config_path}`,
-        ])
-        cmd.stdout.pipe(stream);
-        cmd.stderr.pipe(stream);
+        cmd.stderr.on('data', (data) => {
+          // output
+          if (showLog) {
+            process.stdout.write(data);
+          } else {
+            data = data.toString('utf8');
+            log('err', data);
+          }
+        });
+
+        cmd.stderr.on('close', (code) => {
+          log('close', code);
+          code ? reject(code) : resolve(code);
+        });
       });
-      await lerna_run;
-      console.log('done');
+      log('done');
+      if (!ready && !showLog) {
+        console.log('Something went wrong. Examine the log using --show_log.');
+        process.exit(1);
+      }
     } catch (e) {
-      console.log('error error error');
+      console.log('Error while running the node:');
       console.error(e);
       debugger;
     }
@@ -101,9 +137,13 @@ export const root = branch({
   commandName: 'rise-manager',
   description: `
     Manager your RISE node instance, including docker images.
-    
-    Examples:
-    ./rise-manager node start`,
+
+    Usage:
+    ./rise-manager node start
+    ./rise-manager docker start
+    ./rise-manager node download
+    ./rise-manager docker download
+    `,
   subcommands: [docker, node],
 });
 
