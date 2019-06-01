b6ca0ef9668cc10be480e9c8fc472560d4f53d1f
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2019-06-01 18:34:15 +0200

    - manager script commands   - download & extract a dist bundle

--- packages/rise-manager/src/node.ts
+++ packages/rise-manager/src/node.ts
@@ -1,11 +1,15 @@
 import { leaf, option } from '@carnesen/cli';
-import { exec } from 'child_process';
-import { resolve } from 'path';
+import { exec, execSync } from 'child_process';
+import * as path from 'path';
 import * as debug from 'debug';
 // TODO uncomment for production
 // import * as http from 'https';
 import * as http from 'http';
 import * as fs from 'fs';
+
+const NODE_DIR = 'rise-node';
+const NODE_URL = 'https://github.com/RiseVision/rise-node-priv/releases/';
+const NODE_FILENAME = 'rise-node.tar.gz';
 
 const SEC = 1000;
 const MIN = 60 * SEC;
@@ -34,7 +38,7 @@ export const node_start = leaf({
       defaultValue: false,
       description: 'Keep the process in the foreground. Implies --showLog',
     }),
-    show_log: option({
+    show_logs: option({
       typeName: 'boolean',
       nullable: true,
       defaultValue: false,
@@ -42,20 +46,26 @@ export const node_start = leaf({
     }),
   },
 
-  async action({ config, network, foreground, show_log }) {
-    // TODO check if in the correct dir
-    // TODO check if `./node_modules/.bin/lerna` exists
-    const showLog = show_log || foreground;
-    const config_path = resolve(config);
-    console.log('Stating RISE node...');
+  async action({ config, network, foreground, show_logs }) {
+    if (!checkNodeDirExists() || !checkLernaExists()) {
+      return;
+    }
+    const showLogs = show_logs || foreground;
+    const config_path = path.resolve(config);
+    // remember the original cwd
+    const cwd = process.cwd()
     let ready = false;
+
+    console.log('Stating RISE node...');
     try {
       await new Promise((resolve, reject) => {
         // kill the process after 2 mins
         const timeout = setTimeout(reject.bind(null, 'TIMEOUT'), 2 * MIN);
 
+        process.chdir(path.resolve(__dirname, NODE_DIR));
         const cmd = exec(
-          './node_modules/.bin/lerna run ' +
+          getLernaPath() +
+            ' run ' +
             `start:${network} ` +
             '--stream ' +
             '--no-prefix ' +
@@ -65,7 +75,7 @@ export const node_start = leaf({
 
         cmd.stdout.on('data', (data: Buffer) => {
           // output
-          if (showLog) {
+          if (showLogs) {
             process.stdout.write(data);
           } else {
             log(data.toString('utf8'));
@@ -83,7 +93,7 @@ export const node_start = leaf({
 
         cmd.stderr.on('data', (data: Buffer) => {
           // output
-          if (showLog) {
+          if (showLogs) {
             process.stdout.write(data);
           } else {
             log('err', data.toString('utf8'));
@@ -96,24 +106,50 @@ export const node_start = leaf({
         });
       });
       log('done');
-      if (!ready && !showLog) {
-        console.log('Something went wrong. Examine the log using --show_log.');
+      if (!ready && !showLogs) {
+        console.log('Something went wrong. Examine the log using --show_logs.');
         process.exit(1);
       }
     } catch (e) {
       console.log('Error while running the node:');
       console.error(e);
-      debugger;
+    } finally {
+      process.chdir(cwd)
     }
   },
 });
 
-const NODE_URL = 'https://github.com/RiseVision/rise-node-priv/releases/';
-const NODE_FILENAME = 'rise-node.tar.gz';
+function checkNodeDirExists(): boolean {
+  if (!fs.existsSync(NODE_DIR) || !fs.lstatSync(NODE_DIR).isDirectory()) {
+    console.log(`Error: directory 'rise-node' doesn't exist.`);
+    console.log(`You can download the latest version using:`);
+    console.log(`  ./rise node download`);
+    return false;
+  }
+  return true;
+}
+
+function checkLernaExists(): boolean {
+  const file = getLernaPath();
+  if (!fs.existsSync(file)) {
+    console.log(`Error: can't find lerna exacutable in 'rise-node'.`);
+    console.log(`You can download the latest version using:`);
+    console.log(`  ./rise node download`);
+    return false;
+  }
+  return true;
+}
+
+function getLernaPath(): string {
+  return path.resolve(
+    path.join(__dirname, NODE_DIR, 'node_modules', '.bin', 'lerna')
+  );
+}
 
 export const node_download = leaf({
   commandName: 'download',
-  description: 'Download a node release file',
+  description:
+    'Download a node release file and extract it to the current directory.',
 
   options: {
     version: option({
@@ -125,17 +161,19 @@ export const node_download = leaf({
   },
 
   async action({ version }) {
-    // TODO uncomment for production
-    // const url = NODE_URL + version + '/' + NODE_FILENAME;
-    const url = 'http://localhost:8080/rise-node.tar.gz';
+    const url = process.env['DEV']
+      ? 'http://localhost:8080/rise-node.tar.gz'
+      : NODE_URL + version + '/' + NODE_FILENAME;
+
+    console.log(`Downloading ${url}`);
+
     const file = fs.createWriteStream(NODE_FILENAME);
-    // TODO console msg with the version being downloaded
-    // TODO show progress
+    // TODO show progress ?
     await new Promise((resolve, reject) => {
       http
         .get(url, function(response) {
           response.pipe(file);
-          response.on('finish', function() {
+          file.on('finish', function() {
             file.close();
             resolve();
           });
@@ -146,7 +184,13 @@ export const node_download = leaf({
           });
         });
     });
-    console.log('Download complete');
-    // TODO extract rise-node.tar.gz
+
+    console.log('Download completed');
+    console.log('Extracting rise-node.tar.gz');
+
+    execSync(`tar -zxf rise-node.tar.gz`);
+
+    console.log('Done. You can start the node using:');
+    console.log('  ./rise node start');
   },
 });
