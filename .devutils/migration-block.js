81d5a4cfb8f4de38c189581baf2d8b7f988369b7
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2019-09-05 15:09:30 +0200

    calculate the migration block

---
+++ .devutils/migration-block.js
@@ -0,0 +1,27 @@
+// config
+const hours = 5 * 24;
+const currentHeight = 330246;
+
+// consts
+const secPerBlock = 30;
+
+// blocks
+const blockAhead = (hours * 60 * 60) / secPerBlock;
+let migrationBlock = currentHeight + blockAhead;
+
+let adjustTime = 0;
+
+// adjust
+while ((migrationBlock - 1) % 101) {
+  migrationBlock++;
+  adjustTime += secPerBlock;
+}
+
+// time
+const migrationTime = new Date();
+migrationTime.setTime(
+  migrationTime.getTime() + (hours * 60 * 60 + adjustTime) * 1000
+);
+
+console.log('Migration height:', migrationBlock);
+console.log('Migration time:', migrationTime);
